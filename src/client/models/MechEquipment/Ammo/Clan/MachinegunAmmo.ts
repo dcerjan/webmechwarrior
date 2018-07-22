f6882b955c66e7bd1ddd36e7db895d29e75e6e86
dcerjan <domagoj.cerjan@gmail.com>
2018-07-22 11:17:59 +0200

    Fix amo tonnage for machineguns

--- src/client/models/MechEquipment/Ammo/Clan/MachinegunAmmo.ts
+++ src/client/models/MechEquipment/Ammo/Clan/MachinegunAmmo.ts
@@ -7,11 +7,11 @@ import {
 } from '../AmmoBin'
 
 export const MachinegunAmmo = {
-  [Name.C_MG_Ammo]:       AmmoBin(Name.C_MG_Ammo,       C.Ballistic, T.Bullet, Tech.Clan, 200, 1, 0.5,  4, [], 'Clan Standard Machinegun Ammo Bin'),
-  [Name.C_MG_Half_Ammo]:  AmmoBin(Name.C_MG_Half_Ammo,  C.Ballistic, T.Bullet, Tech.Clan, 100, 1, 0.25, 4, [], 'Clan Standard Machinegun Ammo Bin (1/2)') }
+  [Name.C_MG_Ammo]:       AmmoBin(Name.C_MG_Ammo,       C.Ballistic, T.Bullet, Tech.Clan, 200, 1, 1.0, 4, [], 'Clan Standard Machinegun Ammo Bin'),
+  [Name.C_MG_Half_Ammo]:  AmmoBin(Name.C_MG_Half_Ammo,  C.Ballistic, T.Bullet, Tech.Clan, 100, 1, 0.5, 4, [], 'Clan Standard Machinegun Ammo Bin (1/2)') }
 export const LightMacinegunAmmo = {
-  [Name.C_LMG_Ammo]:      AmmoBin(Name.C_LMG_Ammo,      C.Ballistic, T.Bullet, Tech.Clan, 200, 1, 0.5,  4, [], 'Clan Standard Light Machinegun Ammo Bin'),
-  [Name.C_LMG_Half_Ammo]: AmmoBin(Name.C_LMG_Half_Ammo, C.Ballistic, T.Bullet, Tech.Clan, 100, 1, 0.25, 4, [], 'Clan Standard Light Machinegun Ammo Bin (1/2)') }
+  [Name.C_LMG_Ammo]:      AmmoBin(Name.C_LMG_Ammo,      C.Ballistic, T.Bullet, Tech.Clan, 200, 1, 1.0, 4, [], 'Clan Standard Light Machinegun Ammo Bin'),
+  [Name.C_LMG_Half_Ammo]: AmmoBin(Name.C_LMG_Half_Ammo, C.Ballistic, T.Bullet, Tech.Clan, 100, 1, 0.5, 4, [], 'Clan Standard Light Machinegun Ammo Bin (1/2)') }
 export const HeavyMachinegunAmmo = {
-  [Name.C_HMG_Ammo]:      AmmoBin(Name.C_HMG_Ammo,      C.Ballistic, T.Bullet, Tech.Clan, 100, 1, 1.0,  4, [], 'Clan Standard Heavy Machinegun Ammo Bin'),
-  [Name.C_HMG_Half_Ammo]: AmmoBin(Name.C_HMG_Half_Ammo, C.Ballistic, T.Bullet, Tech.Clan,  50, 1, 0.5,  4, [], 'Clan Standard Heavy Machinegun Ammo Bin (1/2)') }
+  [Name.C_HMG_Ammo]:      AmmoBin(Name.C_HMG_Ammo,      C.Ballistic, T.Bullet, Tech.Clan, 100, 1, 1.0, 4, [], 'Clan Standard Heavy Machinegun Ammo Bin'),
+  [Name.C_HMG_Half_Ammo]: AmmoBin(Name.C_HMG_Half_Ammo, C.Ballistic, T.Bullet, Tech.Clan,  50, 1, 0.5, 4, [], 'Clan Standard Heavy Machinegun Ammo Bin (1/2)') }
--- src/client/models/MechEquipment/Ammo/InnerSphere/MachinegunAmmo.ts
+++ src/client/models/MechEquipment/Ammo/InnerSphere/MachinegunAmmo.ts
@@ -7,11 +7,11 @@ import {
 } from '../AmmoBin'
 
 export const MachinegunAmmo = {
-  [Name.IS_MG_Ammo]:       AmmoBin(Name.IS_MG_Ammo,       C.Ballistic, T.Bullet, Tech.IS, 200, 1, 0.5,  4, [], 'Inner Sphere Standard Machinegun Ammo Bin'),
-  [Name.IS_MG_Half_Ammo]:  AmmoBin(Name.IS_MG_Half_Ammo,  C.Ballistic, T.Bullet, Tech.IS, 100, 1, 0.25, 4, [], 'Inner Sphere Standard Machinegun Ammo Bin (1/2)') }
+  [Name.IS_MG_Ammo]:       AmmoBin(Name.IS_MG_Ammo,       C.Ballistic, T.Bullet, Tech.IS, 200, 1, 1.0, 4, [], 'Inner Sphere Standard Machinegun Ammo Bin'),
+  [Name.IS_MG_Half_Ammo]:  AmmoBin(Name.IS_MG_Half_Ammo,  C.Ballistic, T.Bullet, Tech.IS, 100, 1, 0.5, 4, [], 'Inner Sphere Standard Machinegun Ammo Bin (1/2)') }
 export const LightMachinegunAmmo = {
-  [Name.IS_LMG_Ammo]:      AmmoBin(Name.IS_LMG_Ammo,      C.Ballistic, T.Bullet, Tech.IS, 200, 1, 0.5,  4, [], 'Inner Sphere Standard Light Machinegun Ammo Bin'),
-  [Name.IS_LMG_Half_Ammo]: AmmoBin(Name.IS_LMG_Half_Ammo, C.Ballistic, T.Bullet, Tech.IS, 100, 1, 0.25, 4, [], 'Inner Sphere Standard Light Machinegun Ammo Bin (1/2)') }
+  [Name.IS_LMG_Ammo]:      AmmoBin(Name.IS_LMG_Ammo,      C.Ballistic, T.Bullet, Tech.IS, 200, 1, 1.0, 4, [], 'Inner Sphere Standard Light Machinegun Ammo Bin'),
+  [Name.IS_LMG_Half_Ammo]: AmmoBin(Name.IS_LMG_Half_Ammo, C.Ballistic, T.Bullet, Tech.IS, 100, 1, 0.5, 4, [], 'Inner Sphere Standard Light Machinegun Ammo Bin (1/2)') }
 export const HeavyMachinegunAmmo = {
-  [Name.IS_HMG_Ammo]:      AmmoBin(Name.IS_HMG_Ammo,      C.Ballistic, T.Bullet, Tech.IS, 100, 1, 1.0,  4, [], 'Inner Sphere Standard Heavy Machinegun Ammo Bin'),
-  [Name.IS_HMG_Half_Ammo]: AmmoBin(Name.IS_HMG_Half_Ammo, C.Ballistic, T.Bullet, Tech.IS,  50, 1, 0.5,  4, [], 'Inner Sphere Standard Heavy Machinegun Ammo Bin (1/2)') }
+  [Name.IS_HMG_Ammo]:      AmmoBin(Name.IS_HMG_Ammo,      C.Ballistic, T.Bullet, Tech.IS, 100, 1, 1.0, 4, [], 'Inner Sphere Standard Heavy Machinegun Ammo Bin'),
+  [Name.IS_HMG_Half_Ammo]: AmmoBin(Name.IS_HMG_Half_Ammo, C.Ballistic, T.Bullet, Tech.IS,  50, 1, 0.5, 4, [], 'Inner Sphere Standard Heavy Machinegun Ammo Bin (1/2)') }
