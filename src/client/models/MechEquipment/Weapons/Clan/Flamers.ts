0072cc70a3b545ebdbf0ffba433c96631a669499
dcerjan <domagoj.cerjan@gmail.com>
2018-07-23 11:05:34 +0200

    Fix flamer tonnage, fix names

--- src/client/models/MechEquipment/Weapons/Clan/Flamers.ts
+++ src/client/models/MechEquipment/Weapons/Clan/Flamers.ts
@@ -8,5 +8,5 @@ import {
 
 
 export const Flamers = {
-  [Name.C_Flamer]:         Weapon(Name.C_Flamer,         C.Energy, T.Flamer, Tech.IS, [0, 1, 2, 3], [2, 2, 2], [3, 3, 3], 3, 1.0, 1, 0, [],                        [], 'Inner Sphere Flamer'),
+  [Name.C_Flamer]:         Weapon(Name.C_Flamer,         C.Energy, T.Flamer, Tech.IS, [0, 1, 2, 3], [2, 2, 2], [3, 3, 3], 3, 0.5, 1, 0, [], [], 'Inner Sphere Flamer'),
   [Name.C_Vehicle_Flamer]: Weapon(Name.C_Vehicle_Flamer, C.Energy, T.Flamer, Tech.IS, [0, 1, 2, 3], [2, 2, 2], [3, 3, 3], 0, 0.5, 1, 0, [Name.C_Flamer_Ammo], [], 'Inner Sphere Vehicle Flamer') }
